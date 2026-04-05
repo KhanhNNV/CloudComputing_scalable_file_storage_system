bc2fc8f73334347c9dc508525388280b6922276f
Binh <dovanbinh1512@gmail.com>
2026-04-05 17:08:44 +0700

    bosung

--- backend/src/main/java/com/uth/backend/dto/response/FileResponse.java
+++ backend/src/main/java/com/uth/backend/dto/response/FileResponse.java
@@ -21,4 +21,6 @@ public class FileResponse {
     private String mimeType;
     private boolean isDeleted;
     private LocalDateTime createdAt;
+    private String downloadUrl;
+    private String thumbnailUrl;
 }
